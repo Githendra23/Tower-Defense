cb2e0eba46d5568ca3be186f0247bf957be3b58c
Julian <145970631+Julian-Schne@users.noreply.github.com>
2023-11-07 17:17:30 +0100

    [~]HomingRocket-> made it so it won't orbit

--- core/src/com/towerdefense/game/HomingRocket.java
+++ core/src/com/towerdefense/game/HomingRocket.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class HomingRocket extends Projectile
 {
-    private int speedX=0;
-    private int speedY=0;
+    private float speedX=0;
+    private float speedY=0;
     public HomingRocket(int positionX, int positionY) {
         super(positionX, positionY, 2,2, new Texture("badlogic.jpg"));
 //        homing(Gdx.input.getX() - (((float) img.getHeight()) / 2), -Gdx.input.getY() + (Gdx.graphics.getHeight() - (((float) img.getWidth()) / 2)));
@@ -18,16 +18,18 @@ public class HomingRocket extends Projectile
     }
 
     public void homing(float targetX, float targetY){
-//        if (speedX<0)speedX++;
-//        if (speedX>0)speedX--;
-//        if (speedY<0)speedY++;
-//        if (speedY>0)speedY--;
-        if (positionX<targetX && speedX<100) speedX++;
-        if (positionX>targetX && speedX>-100) speedX--;
-        if (positionY<targetY&& speedY<100) speedY++;
-        if(positionY>targetY&& speedY>-100) speedY--;
-        positionX+=speedX/2;
-        positionY+=speedY/2;
+        if (speedX<0)speedX+=0.05f;
+        if (speedX>0)speedX-=0.05f;
+        if (speedY<0)speedY+=0.05f;
+        if (speedY>0)speedY-=0.05f;
+        if (positionX<targetX) speedX++;
+        if (positionX>targetX) speedX--;
+        if (positionY<targetY) speedY++;
+        if(positionY>targetY) speedY--;
+//        if (targetX-positionX>targetY-positionY) speedX=(targetX-positionX)/5;
+//        if (targetX-positionX<targetY-positionY) speedY=(targetY-positionY)/5;
+        positionX+=speedX;
+        positionY+=speedY;
 
     }
 }
