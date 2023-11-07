0d243818486deb7f91617c77d6eb7a963663a0f5
Julian <145970631+Julian-Schne@users.noreply.github.com>
2023-11-07 17:16:51 +0100

    [+]Added bullet

---
+++ core/src/com/towerdefense/game/Bullet.java
@@ -0,0 +1,27 @@
+package com.towerdefense.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Bullet extends Projectile{
+    private float speedX=0;
+    private float speedY=0;
+    public Bullet(int positionX, int positionY) {
+        super(positionX, positionY, 2,2, new Texture("badlogic.jpg"));
+//        homing(Gdx.input.getX() - (((float) img.getHeight()) / 2), -Gdx.input.getY() + (Gdx.graphics.getHeight() - (((float) img.getWidth()) / 2)));
+    }
+
+    public Texture drawRocket()
+    {
+        return img;
+    }
+
+    public void shoot(float targetX, float targetY,int speed){
+
+        if (positionX<targetX) positionX+=speed;
+        if (positionX>targetX) positionX-=speed;
+        if (positionY<targetY) positionY+=speed;
+        if (positionY>targetY) positionY-=speed;
+
+
+    }
+}
